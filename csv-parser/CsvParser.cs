4e2d9100408c0e90b7a0e6588a7a57921289d3a7
Sam <snippetsbysam@outlook.com>
2022-07-18 04:28:20 +0100

    Add model and basic functions

---
+++ csv-parser/CsvParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv_parser
+{
+    public class CsvParser
+    {
+        public virtual List<T> Parse<T>(string data, string delimiter)
+        {
+            List<T> items = new List<T>();
+            return items;
+        }
+
+        public virtual string[] GetHeaders(string header, string delimiter)
+        {
+            string[] test = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            return test;
+        }
+    }
+}
---
+++ csv-parser/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv_parser.Models
+{
+    public class Person
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string company_name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string county { get; set; }
+        public string postal { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string email { get; set; }
+        public string web { get; set; }
+    }
+}
